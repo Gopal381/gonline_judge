08fa40d8c047c7a21c94d0f5a06961b1d116a51e
Gopal381 <gopalranjan03082001@gmail.com>
2024-08-10 19:54:14 +0530

    create executeCppFile with inputs

--- backend/controllers/generateCpp.js
+++ backend/controllers/generateCpp.js
@@ -28,3 +28,27 @@ const executeCpp = async (filepath) => {
     );
   });
 };
+
+//Generate executable file with input
+const executeCppWithInput = async (filepath, inputFile) => {
+  const fileId = path.basename(filepath).split(".")[0];
+  const outputFilename = `${fileId}.exe`;
+  const outputFilePath = path.join(dirOutput, outputFilename);
+
+  return new Promise((resolve, reject) => {
+    exec(
+      `g++ ${filepath} -o ${outputFilePath} && cd ${dirOutput} && ./${outputFilename} < ${inputFile}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        }
+        if (stderr) {
+          reject(stderr);
+        }
+        resolve(stdout);
+      }
+    );
+  });
+};
+
+module.exports = { executeCpp, executeCppWithInput };
